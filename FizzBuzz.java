5d9c21a6cc1f8279cb390cb9056fbf6dde6dfcde
Drew Keller <drew@eightycolumns.com>
2017-08-08 20:25:21 -0400

    Implement FizzBuzz

---
+++ FizzBuzz.java
@@ -0,0 +1,12 @@
+class FizzBuzz {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 100; i += 1) {
+      String s = "";
+
+      if (i % 3 == 0) s += "Fizz";
+      if (i % 5 == 0) s += "Buzz";
+
+      System.out.println(s.isEmpty() ? i : s);
+    }
+  }
+}
