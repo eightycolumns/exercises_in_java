e4cc253cf8b1348696c0b0be5c6c8851ba5ac721
Drew Keller <drew@eightycolumns.com>
2017-08-09 20:24:08 -0400

    Use exclusive upper limit

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,6 +1,6 @@
 class FizzBuzz {
   public static void main(String[] args) {
-    for (int i = 1; i <= 100; i += 1) {
+    for (int i = 1; i < 101; i += 1) {
       String s = "";
 
       if (i % 3 == 0) s += "Fizz";
