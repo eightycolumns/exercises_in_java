277c0280c5ac55b64013cb5a6eddf34a6f6957b9
Drew Keller <drew@eightycolumns.com>
2017-08-10 20:58:44 -0400

    Simplify conditionals with ternary operator

--- Checkerboard.java
+++ Checkerboard.java
@@ -2,11 +2,7 @@ class Checkerboard {
   public static void main(String[] args) {
     for (int i = 0; i < 8; i += 1) {
       for (int j = 0; j < 8; j += 1) {
-        if (is_even(i + j)) {
-          System.out.print("*");
-        } else {
-          System.out.print(" ");
-        }
+        System.out.print(is_even(i + j) ? "*" : " ");
       }
 
       System.out.print("\n");
