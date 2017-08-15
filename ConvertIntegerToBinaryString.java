e972bd5c7c788579f37cc3b1ccc48b9599036dcb
Drew Keller <drew@eightycolumns.com>
2017-08-15 19:49:22 -0400

    Check that integer is nonnegative

--- ConvertIntegerToBinaryString.java
+++ ConvertIntegerToBinaryString.java
@@ -1,11 +1,21 @@
 class ConvertIntegerToBinaryString {
   public static void main(String[] args) {
-    for (int i = 0; i < 256; i += 1) {
-      System.out.format("%3d\t%8s\n", i, toBinaryString(i));
+    try {
+      for (int i = 0; i < 256; i += 1) {
+        System.out.format("%3d\t%8s\n", i, toBinaryString(i));
+      }
+    } catch (IllegalArgumentException e) {
+      System.out.println(e.getClass().getSimpleName() + ": " + e.getMessage());
     }
   }
 
   private static String toBinaryString(int d) {
+    if (d < 0) {
+      throw new IllegalArgumentException(
+        "toBinaryString() expects nonnegative integer"
+      );
+    }
+
     if (d == 0) {
       return "0";
     }
