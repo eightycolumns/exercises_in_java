e9fa46a7988a2e4ff7d281b5072c96addced78fa
Drew Keller <drew@eightycolumns.com>
2017-08-15 19:25:54 -0400

    Extract toBinaryString()

--- ConvertIntegerToBinaryString.java
+++ ConvertIntegerToBinaryString.java
@@ -1,19 +1,21 @@
 class ConvertIntegerToBinaryString {
   public static void main(String[] args) {
     for (int i = 0; i < 256; i += 1) {
-      StringBuilder binaryString;
-
-      if (i == 0) {
-        binaryString = new StringBuilder("0");
-      } else {
-        binaryString = new StringBuilder();
-
-        for (int j = i; j > 0; j /= 2) {
-          binaryString.insert(0, j % 2);
-        }
-      }
-
-      System.out.format("%3d\t%8s\n", i, binaryString);
+      System.out.format("%3d\t%8s\n", i, toBinaryString(i));
     }
+  }
+
+  private static String toBinaryString(int d) {
+    if (d == 0) {
+      return "0";
+    }
+
+    StringBuilder binaryString = new StringBuilder();
+
+    for (int i = d; i > 0; i /= 2) {
+      binaryString.insert(0, i % 2);
+    }
+
+    return binaryString.toString();
   }
 }
